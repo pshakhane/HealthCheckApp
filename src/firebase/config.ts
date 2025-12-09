84bc1fe7e50b4aa35bb0a9fef09f9f7a57e35be8
Elia Shakhane <phomolo.shakhane@gmail.com>
2025-12-09 06:27:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "bmi-snapshot-7qens",
+  "appId": "1:1017725446978:web:e4c15f5a4e4455db3f36db",
+  "apiKey": "AIzaSyBV-2i16ZQZ1zu5ot6xmi6uFVhXwlc2Mvs",
+  "authDomain": "bmi-snapshot-7qens.firebaseapp.com",
+  "measurementId": "G-FQX79XM09Q",
+  "messagingSenderId": "1017725446978"
+};
